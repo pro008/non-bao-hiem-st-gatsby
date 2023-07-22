136ca152d49aaac0ede26a176c2fe376ea5db34c
Felix Vuong <felix.vuong@deliveree.com>
2023-07-22 10:43:22 +0700

    fix nonscript tag manager

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,15 +5,20 @@ import FloatingButton from "./floating_button"
 import FacebookPluggin from "./FacebookPluggin"
 import UrgentCall from "./UrgentCall"
 
-const Layout = ({ children, props }) => {
+const Layout = ({ children }) => {
   useEffect(() => {
     import("../utils/navbar.js")
   }, [])
 
   return (
     <>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLSJXFGD"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NLSJXFGD" 
+          height="0" 
+          width="0" 
+          style={{display:'none',visibility:'hidden'}} />
+      </noscript>
       <FloatingButton />
       <Header />
       {children}
