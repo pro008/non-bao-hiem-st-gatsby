c1d4d30c88c09634c9faa4d4baac05e66541d1e0
Felix Vuong <felix.vuong@deliveree.com>
2023-07-29 15:57:03 +0700

    remove facebook pluggin

--- src/components/FacebookPluggin.js
+++ src/components/FacebookPluggin.js
@@ -1,10 +1,24 @@
 import React from "react"
 
+// const FacebookPluggin = () => {
+//   return (
+//     <>
+//       <div id="fb-root"></div>
+//       <div id="fb-customer-chat" class="fb-customerchat"></div>
+//       <div 
+//         class="zalo-chat-widget" 
+//         data-oaid="3473601869890654178" 
+//         data-welcome-message="Rất vui khi được hỗ trợ bạn!" 
+//         data-autopopup="0" 
+//         data-width="" 
+//         data-height=""></div>
+//     </>
+//   )
+// }
+
 const FacebookPluggin = () => {
   return (
     <>
-      <div id="fb-root"></div>
-      <div id="fb-customer-chat" class="fb-customerchat"></div>
       <div 
         class="zalo-chat-widget" 
         data-oaid="3473601869890654178" 
