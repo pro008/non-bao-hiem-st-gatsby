9133c72ac2ad9a5556731a12375f72b049ad8b69
Felix Vuong <felix.vuong@deliveree.com>
2023-07-29 14:51:14 +0700

    add pluggin

--- src/components/FacebookPluggin.js
+++ src/components/FacebookPluggin.js
@@ -1,15 +1,10 @@
 import React from "react"
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const FacebookPluggin = () => {
   return (
     <>
-      <div>
-        {typeof window !== 'undefiend' && <MessengerCustomerChat
-          pageId="103170092237571"
-          appId="5345237808874287"
-        />}
-      </div>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" class="fb-customerchat"></div>
       <div 
         class="zalo-chat-widget" 
         data-oaid="3473601869890654178" 
