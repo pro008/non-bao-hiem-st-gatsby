198d03ada27b621540c1022cae84e9618e85f515
Felix Vuong <felix.vuong@deliveree.com>
2023-07-29 14:25:09 +0700

    add zalo

--- src/components/FacebookPluggin.js
+++ src/components/FacebookPluggin.js
@@ -1,16 +1,23 @@
 import React from "react"
-import MessengerCustomerChat from "react-messenger-customer-chat"
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const FacebookPluggin = () => {
   return (
-    <div>
-      {typeof window !== "undefiend" && (
-        <MessengerCustomerChat
+    <>
+      <div>
+        {typeof window !== 'undefiend' && <MessengerCustomerChat
           pageId="103170092237571"
           appId="5345237808874287"
-        />
-      )}
-    </div>
+        />}
+      </div>
+      <div 
+        class="zalo-chat-widget" 
+        data-oaid="3473601869890654178" 
+        data-welcome-message="Rất vui khi được hỗ trợ bạn!" 
+        data-autopopup="0" 
+        data-width="" 
+        data-height=""></div>
+    </>
   )
 }
 
